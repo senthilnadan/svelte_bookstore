f5b1869feef8b4fe2ad1702ce30a393ceadd6230
Your Name <you@example.com>
2021-08-04 11:00:46 +0530

    first commit

---
+++ src/lib/bookgrid.svelte
@@ -0,0 +1,41 @@
+<script>
+    import BookCover from "../common/bookcover.svelte";
+    import Heart from "./Heart.svelte";
+  
+    export let books = [];
+  </script>
+
+<ul>
+    {#each books as book}
+      <li>
+        <BookCover {book} />
+        {#if book.favorite}
+          <div class="heart">
+            <Heart />
+          </div>
+        {/if}
+      </li>
+    {/each}
+  </ul>
+
+  <style>
+    ul {
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(16rem, 2fr));
+      grid-template-columns: repeat(auto-fill, minmax(9rem, 2fr));
+      grid-auto-rows: 12.8rem;
+      gap: var(--spacingMedium);
+      list-style: none;
+      max-width: 100%;
+      padding: 0;
+    }
+    li {
+      position: relative;
+      display: flex;
+    }
+    .heart {
+      position: absolute;
+      bottom: calc(-1 * var(--spacingSmall));
+      right: calc(-1 * var(--spacingSmall));
+    }
+  </style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { onMount } from "svelte";
+    import Button from "../common/button.svelte"
+    import BookGrid from "../lib/bookgrid.svelte";
+    import { httpGet } from "../common/bookapi.js";
+
+  let books = [];
+  onMount(async function() {
+    const { data } = await httpGet("/?_sort=id&_order=desc");
+    books = data;
+  });
+</script>
+<h1>Welcome to Our Bookstore</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button>+ Add Book</Button>
+
+<BookGrid {books} />
